18752fb2bb52759e128d420019cddbc93aff98ed
Carbone <rtcarbone@gmail.com>
2025-07-07 22:25:25 -0300

    feat: annotate CadastrarProdutoUsecaseImpl as a service and update ProdutoController with REST mappings

--- produto-service/src/main/java/com/javorders/produtoservice/application/usecases/impl/CadastrarProdutoUsecaseImpl.java
+++ produto-service/src/main/java/com/javorders/produtoservice/application/usecases/impl/CadastrarProdutoUsecaseImpl.java
@@ -3,7 +3,9 @@ package com.javorders.produtoservice.application.usecases.impl;
 import com.javorders.produtoservice.application.usecases.CadastrarProdutoUsecase;
 import com.javorders.produtoservice.domain.gateways.ProdutoGateway;
 import com.javorders.produtoservice.domain.model.Produto;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CadastrarProdutoUsecaseImpl implements CadastrarProdutoUsecase {
 
     private final ProdutoGateway produtoGateway;
@@ -16,4 +18,5 @@ public class CadastrarProdutoUsecaseImpl implements CadastrarProdutoUsecase {
     public Produto executar(Produto produto) {
         return produtoGateway.salvar(produto);
     }
+
 }
--- produto-service/src/main/java/com/javorders/produtoservice/infrastructure/controller/ProdutoController.java
+++ produto-service/src/main/java/com/javorders/produtoservice/infrastructure/controller/ProdutoController.java
@@ -3,7 +3,10 @@ package com.javorders.produtoservice.infrastructure.controller;
 import com.javorders.produtoservice.application.usecases.CadastrarProdutoUsecase;
 import com.javorders.produtoservice.domain.model.Produto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/produtos")
@@ -19,4 +22,5 @@ public class ProdutoController {
     public ResponseEntity<Produto> cadastrar(@RequestBody Produto produto) {
         return ResponseEntity.ok(cadastrarProdutoUsecase.executar(produto));
     }
+
 }
