a07884d03c5e508eff1c53c562d3065837c7d177
Carbone <rtcarbone@gmail.com>
2025-07-13 13:58:32 -0300

    feat: integrate RabbitMQ for order processing; add RabbitTemplate to PedidoController, configure RabbitMQ settings in application.yml, and simplify NovoPedidoEntryPoint constructor

--- pedido-service/src/main/java/com/javorders/pedidoservice/infrastructure/controller/PedidoController.java
+++ pedido-service/src/main/java/com/javorders/pedidoservice/infrastructure/controller/PedidoController.java
@@ -6,6 +6,7 @@ import com.javorders.pedidoservice.infrastructure.controller.dto.PedidoRequestDT
 import com.javorders.pedidoservice.infrastructure.controller.dto.PedidoResponseDTO;
 import com.javorders.pedidoservice.infrastructure.controller.mapper.PedidoDTOMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,11 +17,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PedidoController {
 
+    private final RabbitTemplate rabbitTemplate;
     private final RegistrarPedidoUsecase registrarPedidoUsecase;
     private final AlterarPedidoUsecase alterarPedidoUsecase;
     private final ConsultarPedidoUsecase consultarPedidoUsecase;
     private final ListarPedidosUsecase listarPedidosUsecase;
     private final DeletarPedidoUsecase deletarPedidoUsecase;
+
+    @PostMapping
+    public ResponseEntity<Void> publicarPedido(@RequestBody PedidoRequestDTO dto) {
+        Pedido pedido = PedidoDTOMapper.toDomain(dto);
+        rabbitTemplate.convertAndSend("novo-pedido-queue", pedido);
+        return ResponseEntity.accepted()
+                .build();
+    }
 
     @PostMapping
     public ResponseEntity<PedidoResponseDTO> registrar(@RequestBody PedidoRequestDTO dto) {
