a2867506bf6fb1b505b25c5cd1da0f3470fb8c4e
Carbone <rtcarbone@gmail.com>
2025-07-14 13:57:51 -0300

    feat: ajuste na validação ao baixar estoque

--- pedido-service/src/main/java/com/javorders/pedidoservice/domain/gateways/EstoqueGateway.java
+++ pedido-service/src/main/java/com/javorders/pedidoservice/domain/gateways/EstoqueGateway.java
@@ -1,8 +1,7 @@
 package com.javorders.pedidoservice.domain.gateways;
 
-import com.javorders.pedidoservice.domain.model.Pedido;
-
 public interface EstoqueGateway {
     void baixarEstoque(String sku, Integer quantidade);
+
     void reporEstoque(String sku, Integer quantidade);
 }
