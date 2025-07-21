f4c10cdd2942aa1c0425636b6666b02d3a20f811
Carbone <rtcarbone@gmail.com>
2025-07-21 20:52:34 -0300

    feat: alteracao para uuid tabela pedidos

--- pedido-service/src/main/resources/db/migration/V1__create_pedidos_table.sql
+++ pedido-service/src/main/resources/db/migration/V1__create_pedidos_table.sql
@@ -1,14 +1,16 @@
-CREATE TABLE pedidos (
-    id SERIAL PRIMARY KEY,
-    cliente_id BIGINT,
-    valor_total NUMERIC(10,2),
-    status VARCHAR(50),
-    uuid_transacao VARCHAR(100)
+CREATE TABLE pedidos
+(
+    id             SERIAL PRIMARY KEY,
+    cliente_id     BIGINT,
+    valor_total    NUMERIC(10, 2),
+    status         VARCHAR(50),
+    uuid_transacao UUID
 );
 
-CREATE TABLE itens_pedido (
-    id SERIAL PRIMARY KEY,
-    sku VARCHAR(100),
+CREATE TABLE itens_pedido
+(
+    id         SERIAL PRIMARY KEY,
+    sku        VARCHAR(100),
     quantidade INTEGER,
-    pedido_id BIGINT REFERENCES pedidos(id) ON DELETE CASCADE
+    pedido_id  BIGINT REFERENCES pedidos (id) ON DELETE CASCADE
 );
