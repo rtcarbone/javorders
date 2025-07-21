e93a3d5ced70fdc15c3479e0ff69a50da4fa5eaa
Carbone <rtcarbone@gmail.com>
2025-07-21 20:27:38 -0300

    feat: alterar uuidTransacao type from String to UUID in PedidoEntity

--- pedido-service/src/main/java/com/javorders/pedidoservice/infrastructure/persistence/entity/PedidoEntity.java
+++ pedido-service/src/main/java/com/javorders/pedidoservice/infrastructure/persistence/entity/PedidoEntity.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Table(name = "pedidos")
@@ -30,7 +31,7 @@ public class PedidoEntity {
     private StatusPedido status;
 
     @Column(name = "uuid_transacao")
-    private String uuidTransacao;
+    private UUID uuidTransacao;
 
     @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ItemPedidoEntity> itens;
