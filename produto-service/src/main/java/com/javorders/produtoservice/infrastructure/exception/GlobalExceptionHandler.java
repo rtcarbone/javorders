b000914d366041a75ac391b201faad764b5e3ec3
Carbone <rtcarbone@gmail.com>
2025-07-21 20:57:10 -0300

    feat: add cliente-service dependency to pom.xml and remove ErroResponse import from GlobalExceptionHandler

--- produto-service/src/main/java/com/javorders/produtoservice/infrastructure/exception/GlobalExceptionHandler.java
+++ produto-service/src/main/java/com/javorders/produtoservice/infrastructure/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.javorders.produtoservice.infrastructure.exception;
 
-import com.javorders.clienteservice.infrastructure.exception.ErroResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
