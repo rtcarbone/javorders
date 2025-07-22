83a867ee90a97d8b95e83c6e942db9e1ee07a05a
Carbone <rtcarbone@gmail.com>
2025-07-22 19:42:17 -0300

    feat: add integration tests for Cliente component and gateway using Testcontainers

---
+++ cliente-service/src/test/java/com/javorders/clienteservice/component/ClienteComponentTestContainer.java
@@ -0,0 +1,71 @@
+package com.javorders.clienteservice.component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.javorders.clienteservice.infrastructure.persistence.repository.ClienteRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Testcontainers
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class ClienteComponentTestContainer {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
+            .withDatabaseName("clientes")
+            .withUsername("user")
+            .withPassword("password");
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void deveCadastrarCliente() throws Exception {
+        var requestJson = """
+                    {
+                        "nome": "Test Container",
+                        "cpf": "98765432100",
+                        "dataNascimento": "2000-12-01",
+                        "enderecos": []
+                    }
+                """;
+
+        mockMvc.perform(post("/clientes")
+                                .contentType(APPLICATION_JSON)
+                                .content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.cpf").value("98765432100"));
+
+        var clientes = clienteRepository.findAll();
+        assertThat(clientes).hasSize(1);
+    }
+}
